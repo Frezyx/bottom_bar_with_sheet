65fadbad0d99bc1cdd1541b3744bf58302da5b06
Frezyx <youngfrezyx@gmail.com>
2020-12-03 18:07:28 +0300

    update examples

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:bottom_bar_with_sheet/bottom_bar_with_sheet.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -20,37 +22,152 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  bool _isOpened = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFFEEEE),
       appBar: AppBar(
         title: Text('bottom_bar_with_sheet v0.5.1',
-            style: TextStyle(color: Colors.white)),
+            style: TextStyle().copyWith(color: Colors.white)),
+        backgroundColor: Color(0xFFFF8D8D),
       ),
-      body: Center(child: Text("Place for your content")),
+      body: Center(
+          child: Text("Place for your content",
+              style: TextStyle().copyWith(color: Colors.black))),
       bottomNavigationBar: BottomBarWithSheet(
         selectedIndex: _selectedIndex,
-        sheetChild: Center(child: Text("Place for your another content")),
+        sheetChild: _buildSheetChild(),
+        disableMainActionButton: true,
+        isOpened: _isOpened,
+        curve: Curves.easeIn,
         bottomBarTheme: BottomBarTheme(
-          mainButtonPosition: MainButtonPosition.Middle,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15.0),
+              topRight: Radius.circular(15.0),
+            ),
+          ),
+          backgroundColor: Color(0xFFFF8D8D),
+          selectedItemBackgroundColor: Color(0xFFFF8D8D),
+          selectedItemIconColor: Colors.white,
+          itemIconColor: Colors.white,
+          itemLabelColor: Colors.white,
+          selectedItemLabelColor: Colors.white,
+          itemTextStyle:
+              TextStyle().copyWith(fontSize: 10, fontWeight: FontWeight.w300),
+          heightOpened: 250,
+          height: 0.0,
+          heightClosed: 0.0,
         ),
-        mainActionButtonTheme: MainActionButtonTheme(
-          size: 55,
-          icon: Icon(
-            Icons.add,
+        mainActionButtonTheme:
+            MainActionButtonTheme(icon: Icon(Icons.arrow_upward_outlined)),
+        onSelectItem: (index) => setState(() => _selectedIndex = index),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFFFF8D8D),
+        onPressed: () => setState(() => _isOpened = !_isOpened),
+        child: Icon(
+          _isOpened
+              ? Icons.arrow_downward_outlined
+              : Icons.arrow_upward_outlined,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSheetChild() {
+    return Center(
+      child: Wrap(
+        spacing: 30.0,
+        children: <Widget>[
+          _buildWrapItem(Icons.chat_bubble_outline, "New Chat"),
+          _buildWrapItem(Icons.search_outlined, "Search"),
+          _buildWrapItem(Icons.cloud_outlined, "Backup"),
+          _buildWrapItem(Icons.info_outline_rounded, "Info"),
+          _buildWrapItem(Icons.battery_charging_full_outlined, "Charging"),
+          _buildWrapItem(Icons.settings_outlined, "Settings"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWrapItem(IconData icon, String title) {
+    return SizedBox(
+      width: 90.0,
+      height: 90.0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
             color: Colors.white,
             size: 35,
           ),
-        ),
-        onSelectItem: (index) => setState(() => _selectedIndex = index),
-        items: [
-          BottomBarWithSheetItem(icon: Icons.people),
-          BottomBarWithSheetItem(icon: Icons.shopping_cart),
-          BottomBarWithSheetItem(icon: Icons.settings),
-          BottomBarWithSheetItem(icon: Icons.favorite),
+          Text(
+            title,
+            style: TextStyle().copyWith(color: Colors.white, fontSize: 14),
+          ),
         ],
       ),
     );
   }
 }
+
+// import 'package:bottom_bar_with_sheet/bottom_bar_with_sheet.dart';
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _selectedIndex = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('bottom_bar_with_sheet v0.5.1',
+//             style: TextStyle(color: Colors.white)),
+//       ),
+//       body: Center(child: Text("Place for your content")),
+//       bottomNavigationBar: BottomBarWithSheet(
+//         selectedIndex: _selectedIndex,
+//         sheetChild: Center(child: Text("Place for your another content")),
+//         bottomBarTheme: BottomBarTheme(
+//           mainButtonPosition: MainButtonPosition.Middle,
+//         ),
+//         mainActionButtonTheme: MainActionButtonTheme(
+//           size: 55,
+//           icon: Icon(
+//             Icons.add,
+//             color: Colors.white,
+//             size: 35,
+//           ),
+//         ),
+//         onSelectItem: (index) => setState(() => _selectedIndex = index),
+//         items: [
+//           BottomBarWithSheetItem(icon: Icons.people),
+//           BottomBarWithSheetItem(icon: Icons.shopping_cart),
+//           BottomBarWithSheetItem(icon: Icons.settings),
+//           BottomBarWithSheetItem(icon: Icons.favorite),
+//         ],
+//       ),
+//     );
+//   }
+// }
