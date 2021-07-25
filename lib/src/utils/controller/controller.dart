4de962e94443f3f0a84a8e66af2ba0fb2937f053
Stanislav Ilin <youngfrezyx@gmail.com>
2021-07-25 15:48:38 +0500

    Add sheet controller for bottom bar controller

--- lib/src/utils/controller/controller.dart
+++ lib/src/utils/controller/controller.dart
@@ -3,15 +3,21 @@ import 'dart:async';
 class BottomBarWithSheetController {
   BottomBarWithSheetController({
     required int initialIndex,
+    bool sheetOpened = false,
     this.onItemSelect,
-  }) : _selectedIndex = initialIndex;
+  })  : _selectedIndex = initialIndex,
+        _sheetOpened = sheetOpened;
   final _itemsController = StreamController<int>.broadcast();
+  final _bottomSheetController = StreamController<bool>.broadcast();
 
   Function(int)? onItemSelect;
   int _selectedIndex;
+  bool _sheetOpened;
 
   Stream<int> get itemsStream => _itemsController.stream;
+  Stream<bool> get stream => _bottomSheetController.stream;
   int get selectedIndex => _selectedIndex;
+  bool get isOpened => _sheetOpened;
 
   void selectItem(int val) {
     _selectedIndex = val;
@@ -21,7 +27,13 @@ class BottomBarWithSheetController {
     }
   }
 
+  void toggleSheet() {
+    _sheetOpened = !_sheetOpened;
+    _bottomSheetController.add(_sheetOpened);
+  }
+
   Future<void> close() async {
     await _itemsController.close();
+    await _bottomSheetController.close();
   }
 }
