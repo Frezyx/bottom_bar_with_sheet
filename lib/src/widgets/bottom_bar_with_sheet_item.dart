85eddf8df0a0a9e8548dde6733083b73c88c7c9f
Stanislav Ilin <youngfrezyx@gmail.com>
2021-07-25 12:00:01 +0500

    Remove unused fields

--- lib/src/widgets/bottom_bar_with_sheet_item.dart
+++ lib/src/widgets/bottom_bar_with_sheet_item.dart
@@ -10,40 +10,29 @@ const defaultDuration = Duration(milliseconds: 500);
 
 // ignore: must_be_immutable
 class BottomBarWithSheetItem extends StatelessWidget {
+  const BottomBarWithSheetItem({
+    Key? key,
+    this.label,
+    required this.icon,
+    this.noSelectionState = false,
+  }) : super(key: key);
+
   final String? label;
   final IconData icon;
-  final Duration animationDuration;
 
   /// If [noSelectionState] is true then no styling/state change happens when this item is pressed/selected
   final bool noSelectionState;
-  Color? selectedBackgroundColor;
-  Color? selectedLabelColor;
-  bool? isLeft;
-  Color? itemIconColor;
-
-  BottomBarWithSheetItem({
-    Key? key,
-    this.label,
-    this.selectedBackgroundColor,
-    required this.icon,
-    this.animationDuration = defaultDuration,
-    this.noSelectionState = false,
-    this.itemIconColor,
-  }) : super(key: key);
 
   //TODO: title
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: animationDuration,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(Icons.ac_unit),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon),
+      ],
     );
   }
 }
