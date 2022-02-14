c688b271fdbcd83fb1edc6a311d2415ab20ec38c
Stanislav Ilin <youngfrezyx@gmail.com>
2022-02-14 17:49:31 +0500

    Add more cases

--- test/utils/controller/controller_test.dart
+++ test/utils/controller/controller_test.dart
@@ -9,47 +9,71 @@ void main() {
       _controller = BottomBarWithSheetController(initialIndex: 0);
     });
 
-    test('selectItem', () async {
+    test('selectItem', () {
       _controller.selectItem(2);
 
       expect(_controller.selectedIndex, isNotNull);
       expect(_controller.selectedIndex, 2);
     });
 
-    test('openSheet', () async {
-      _controller.openSheet();
-
-      expect(_controller.isOpened, isNotNull);
-      expect(_controller.isOpened, true);
-
-      _controller.openSheet();
-
-      expect(_controller.isOpened, isNotNull);
-      expect(_controller.isOpened, true);
+    test('openSheet', () {
+      _testOpen(_controller);
+      _testOpen(_controller);
     });
 
-    test('closeSheet', () async {
-      _controller.closeSheet();
-
-      expect(_controller.isOpened, isNotNull);
-      expect(_controller.isOpened, false);
-
-      _controller.closeSheet();
-
-      expect(_controller.isOpened, isNotNull);
-      expect(_controller.isOpened, false);
+    test('openSheet closeSheet', () {
+      _testOpen(_controller);
+      _testOpen(_controller);
     });
 
-    test('toggleSheet', () async {
-      _controller.toggleSheet();
+    test('openSheet 2 times', () {
+      _testOpen(_controller);
+      _testOpen(_controller);
+    });
 
-      expect(_controller.isOpened, isNotNull);
-      expect(_controller.isOpened, true);
+    test('closeSheet', () {
+      _testClose(_controller);
+      _testClose(_controller);
+    });
 
-      _controller.toggleSheet();
+    test('closeSheet openSheet', () {
+      _testClose(_controller);
+      _testOpen(_controller);
+    });
 
-      expect(_controller.isOpened, isNotNull);
-      expect(_controller.isOpened, false);
+    test('closeSheet 2 times', () {
+      _testClose(_controller);
+      _testClose(_controller);
+    });
+
+    test('toggleSheet', () {
+      _testToggle(_controller);
     });
   });
+}
+
+void _testToggle(BottomBarWithSheetController _controller) {
+  _controller.toggleSheet();
+
+  expect(_controller.isOpened, isNotNull);
+  expect(_controller.isOpened, true);
+
+  _controller.toggleSheet();
+
+  expect(_controller.isOpened, isNotNull);
+  expect(_controller.isOpened, false);
+}
+
+void _testClose(BottomBarWithSheetController _controller) {
+  _controller.closeSheet();
+
+  expect(_controller.isOpened, isNotNull);
+  expect(_controller.isOpened, false);
+}
+
+void _testOpen(BottomBarWithSheetController _controller) {
+  _controller.openSheet();
+
+  expect(_controller.isOpened, isNotNull);
+  expect(_controller.isOpened, true);
 }
