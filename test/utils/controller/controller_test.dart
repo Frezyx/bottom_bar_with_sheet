f53de15c215a7a365127abf8378a448dcc9c7547
Stanislav Ilin <youngfrezyx@gmail.com>
2022-02-14 17:46:48 +0500

    Add open and close tests

--- test/utils/controller/controller_test.dart
+++ test/utils/controller/controller_test.dart
@@ -2,21 +2,45 @@ import 'package:bottom_bar_with_sheet/src/src.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Test_Controller', () {
+  group('BottomBarWithSheetController', () {
     late BottomBarWithSheetController _controller;
 
     setUp(() {
       _controller = BottomBarWithSheetController(initialIndex: 0);
     });
 
-    test('Select_Item', () async {
+    test('selectItem', () async {
       _controller.selectItem(2);
 
       expect(_controller.selectedIndex, isNotNull);
       expect(_controller.selectedIndex, 2);
     });
 
-    test('Toggle_Sheet', () async {
+    test('openSheet', () async {
+      _controller.openSheet();
+
+      expect(_controller.isOpened, isNotNull);
+      expect(_controller.isOpened, true);
+
+      _controller.openSheet();
+
+      expect(_controller.isOpened, isNotNull);
+      expect(_controller.isOpened, true);
+    });
+
+    test('closeSheet', () async {
+      _controller.closeSheet();
+
+      expect(_controller.isOpened, isNotNull);
+      expect(_controller.isOpened, false);
+
+      _controller.closeSheet();
+
+      expect(_controller.isOpened, isNotNull);
+      expect(_controller.isOpened, false);
+    });
+
+    test('toggleSheet', () async {
       _controller.toggleSheet();
 
       expect(_controller.isOpened, isNotNull);
