16dde4b865d2f20b78881e5fc771bf9c1c290675
Stanislav Ilin <youngfrezyx@gmail.com>
2022-05-03 01:24:23 +0500

    Delete old example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:bottom_bar_with_sheet/bottom_bar_with_sheet.dart';
-import 'package:example/examples/without_background_bar_items_example.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const WithoutBackExample());
+void main() => runApp(const BaseExample());
 
 class BaseExample extends StatelessWidget {
   const BaseExample({Key? key}) : super(key: key);
